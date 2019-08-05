c3baeae810f70ef7c10dfce71d4777320d169193
Jon Karlsen <karlsen.jonarild@gmail.com>
2019-08-05 12:58:08 +0200

    Chore: move ProductServiceTests example methods => example class

--- P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -11,92 +11,95 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
-        private P3Referential _context;
-        public ProductServiceTests()
+        public class ExampleTestMethods
         {
-            var options = new DbContextOptionsBuilder<P3Referential>().
-                       UseSqlServer("Server=.;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
-                       .Options;
-
-            _context = new P3Referential(options);
-        }
-
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
-        [Fact]
-        public async Task TestGetProductByIdAsyncMockTest()
-        {
-            // Arrange
-            //      public int Id { get; set; }
-            //public string Description { get; set; }
-            //public string Details { get; set; }
-            //public string Name { get; set; }
-            //public double Price { get; set; }
-            //public int Quantity { get; set; }
-
-            //public virtual ICollection<OrderLine> OrderLine { get; set; }
-
-            Product product = new Product
+            private P3Referential _context;
+            public ExampleTestMethods()
             {
-                Id = 1,
-                Description = "one",
-                Details = "onedetails",
+                var options = new DbContextOptionsBuilder<P3Referential>().
+                           UseSqlServer("Server=.;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                           .Options;
 
-            };
+                _context = new P3Referential(options);
+            }
 
-            var mockProductRepository = new Mock<IProductRepository>();
-            mockProductRepository.Setup(x => x.GetProduct(1)).Returns(Task.FromResult(product));
-
-            // Act
-            var productService = new ProductService(null, mockProductRepository.Object, null, null);
-            var result = await productService.GetProduct(1);
-            
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal("one", result.Description);
-        }
-
-        [Theory]
-        [InlineData(1, "one", "oneDetails")]
-        [InlineData(2, "two", "twoDetails")]
-        [InlineData(3, "three", "threeDetails")]
-        public async Task TestGetProductByIdAsyncMockTestParameterizedTestData(int id, string description, string details)
-        {
-            // Arrange
-            Product product = new Product
+            /// <summary>
+            /// Take this test method as a template to write your test method.
+            /// A test method must check if a definite method does its job:
+            /// returns an expected value from a particular set of parameters
+            /// </summary>
+            [Fact]
+            public async Task TestGetProductByIdAsyncMockTest()
             {
-                Id = id,
-                Description = description,
-                Details = details,
-            };
+                // Arrange
+                //      public int Id { get; set; }
+                //public string Description { get; set; }
+                //public string Details { get; set; }
+                //public string Name { get; set; }
+                //public double Price { get; set; }
+                //public int Quantity { get; set; }
 
-            var mockProductRepository = new Mock<IProductRepository>();
-            mockProductRepository.Setup(x => x.GetProduct(id)).Returns(Task.FromResult(product));
+                //public virtual ICollection<OrderLine> OrderLine { get; set; }
 
-            // Act
-            var productService = new ProductService(null, mockProductRepository.Object, null, null);
-            var result = await productService.GetProduct(id);
-            
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal(description, result.Description);
-        }
+                Product product = new Product
+                {
+                    Id = 1,
+                    Description = "one",
+                    Details = "onedetails",
 
-        [Fact]
-        public async Task TestGetProductByIdIntegrationTestAsync()
-        {
-            //Arrange
-            ProductRepository productRepository = new ProductRepository(_context);
+                };
 
-            // Act
-            var result = await productRepository.GetProduct(5);
-            
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal("Cell Phone", result.Description);
+                var mockProductRepository = new Mock<IProductRepository>();
+                mockProductRepository.Setup(x => x.GetProduct(1)).Returns(Task.FromResult(product));
+
+                // Act
+                var productService = new ProductService(null, mockProductRepository.Object, null, null);
+                var result = await productService.GetProduct(1);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("one", result.Description);
+            }
+
+            [Theory]
+            [InlineData(1, "one", "oneDetails")]
+            [InlineData(2, "two", "twoDetails")]
+            [InlineData(3, "three", "threeDetails")]
+            public async Task TestGetProductByIdAsyncMockTestParameterizedTestData(int id, string description, string details)
+            {
+                // Arrange
+                Product product = new Product
+                {
+                    Id = id,
+                    Description = description,
+                    Details = details,
+                };
+
+                var mockProductRepository = new Mock<IProductRepository>();
+                mockProductRepository.Setup(x => x.GetProduct(id)).Returns(Task.FromResult(product));
+
+                // Act
+                var productService = new ProductService(null, mockProductRepository.Object, null, null);
+                var result = await productService.GetProduct(id);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal(description, result.Description);
+            }
+
+            [Fact]
+            public async Task TestGetProductByIdIntegrationTestAsync()
+            {
+                //Arrange
+                ProductRepository productRepository = new ProductRepository(_context);
+
+                // Act
+                var result = await productRepository.GetProduct(5);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("Cell Phone", result.Description);
+            }
         }
 
         // TODO write test methods to ensure a correct coverage of all possibilities
